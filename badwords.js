9492d31a7d4c6824fb309ea75317352aa042093f
nayereahmadi1-ux <nayereahmadi1@gmail.com>
2026-05-08 12:41:26 +0330

    Create badwords.js

---
+++ badwords.js
@@ -0,0 +1,5 @@
+export const badWords = [
+  "احمق", "ابله", "عوضی", "خر", "موز", "گاو", "کثافت",
+  "بی‌شعور", "خفه", "برو گم شو", "shit", "fuck", "koskhol", "koskholi",
+  "dajjal", "dumb", "sag", "khar", "ahmaq"
+];
